a110557c9383a45f046313c9ccad69d9ac6999df
CeeMoo <qdale.123@gmail.com>
2021-08-27 20:52:27 +0300

    Initial commit

---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,29 @@
+<?php
+
+$txt['themecopyright'] = ' <strong>ZirzarV2 Theme By <a href="https://smf.konusal.com/" target="_blank">Cee</a></strong>';
+
+
+$txt['bese_color_default'] = 'bese_color_default';
+$txt['bese_color_primary'] = 'bese_color_primary';
+$txt['bese_color_secondary'] = 'bese_color_secondary';
+$txt['bese_default_mode'] = 'bese_default_mode';
+
+$txt['bese_color_defaulte'] = 'Default is: %color%';
+
+
+$txt['bese_default_mode'] = 'Default Mode';
+$txt['bese_dark_mode'] = 'Dark Mode';
+$txt['bese_light_mode'] = 'Light Mode';
+$txt['bese_theme_settings'] = 'Zirzar Theme Settings';
+
+$txt['bese_allow_user_modes'] = 'bese_allow_user_modes';
+$txt['bese_allow_color_select'] = 'bese_allow_color_select';
+
+$txt['zirzar_site_slogan'] = 'ZirzarV2';
+
+$txt['default_bese_color'] = '#da564f';
+$txt['default_bese_primary'] = '#eb7870';
+$txt['default_bese_secondary'] = '#721d18';
+$txt['color_zirzar1'] = '#6e2622';
+
+?>
